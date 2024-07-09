93b9908f2f9ecc422b381135a370c16e0627a9cf
AlxisHenry <alexis.henry150357@gmail.com>
2024-07-09 15:32:39 +0200

    feat: retrieve images on multiple navigators

--- app.js
+++ app.js
@@ -1,29 +1,74 @@
 import puppeteer from 'puppeteer';
-// Or import puppeteer from 'puppeteer-core';
+import fs from 'fs';
+import urls from './urls.json' assert { type: "json" };
+import client from 'https';
 
-// Launch the browser and open a new blank page
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
+const basePath = './images';
 
-// Navigate the page to a URL.
-await page.goto('https://developer.chrome.com/');
+function downloadImage(url, filepath) {
+    return new Promise((resolve, reject) => {
+        client.get(url, (res) => {
+            if (res.statusCode === 200) {
+                res.pipe(fs.createWriteStream(filepath))
+                    .on('error', reject)
+                    .once('close', () => resolve(filepath));
+            } else {
+                // Consume response data to free up memory
+                res.resume();
+                reject(new Error(`Request Failed With a Status Code: ${res.statusCode}`));
 
-// Set screen size.
-await page.setViewport({ width: 1080, height: 1024 });
+            }
+        });
+    });
+}
 
-// Type into search box.
-await page.locator('.devsite-search-field').fill('automate beyond recorder');
+function retrieveImages() {
+    const images = document.querySelectorAll('img');
+    const urls = [];
+    images.forEach((image) => {
+        if (image.width === 12 && image.height === 12) return;
+        if (image.width < 200) return;
+        urls.push(image.src);
+    });
+    return urls;
+}
 
-// Wait and click on first result.
-await page.locator('.devsite-result-item-link').click();
+fs.readdirSync(basePath).forEach((file) => {
+    if (file === '.gitkeep' || file === '.gitignore') return;
+    fs.rmSync(`${basePath}/${file}`, { recursive: true, force: true });
+});
 
-// Locate the full title with a unique string.
-const textSelector = await page
-    .locator('text/Customize and automate')
-    .waitHandle();
-const fullTitle = await textSelector?.evaluate(el => el.textContent);
+for (const key in urls) {
+    if (!fs.existsSync(`${basePath}/${key}`)) {
+        fs.mkdirSync(`${basePath}/${key}`);
+    }
 
-// Print the full title.
-console.log('The title of this blog post is "%s".', fullTitle);
+    const browser = await puppeteer.launch({
+        headless: "new",
+    });
 
-await browser.close();
+    const page = await browser.newPage();
+
+    await page.goto(urls[key], { waitUntil: 'networkidle2' });
+
+    const images = await page.evaluate(retrieveImages);
+
+    images.forEach(async (url, index) => {
+        if (!url.includes('base64')) {
+            downloadImage(url, `./${basePath}/${key}/image${index}.png`)
+                .then((filepath) => {
+                    console.log(`Downloaded File: ${filepath}`);
+                })
+                .catch((err) => {
+                    console.error(`Failed to Download File: ${err}`);
+                });
+        } else {
+            const base64Image = url.split(';base64,').pop();
+            if (!base64Image) return;
+            fs.writeFileSync(`./${basePath}/${key}/image${index}.png`, base64Image, { encoding: 'base64' });
+            console.log(`Downloaded File: ./images/${key}/image${index}.png (base64)`);
+        }
+    });
+
+    await browser.close();
+}
